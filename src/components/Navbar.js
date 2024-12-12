1e5424eb9ebb55390c22810b4593946ac8f465e8
Guhan Manickavasagam <mguhan1999@gmail.com>
2024-12-12 15:47:40 +0530

    update project

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+
+// creating navbar component
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Perfumy</h1>
+      <ul>
+        <li>Home |</li>
+        <li>Product |</li>
+        <li>About |</li>
+        <li>Contect </li>
+
+      </ul>
+    </nav>
+  )
+}
+
+
+export default Navbar
